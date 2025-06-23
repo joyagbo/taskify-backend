f59bcc51cd3142e233fdcee296f095681af621c9
joyagbo <joyagbo49@gmail.com>
2025-06-23 02:15:44 +0100

    Setup task routes and link to controller functions

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,2 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+const router = express.Router();
+const { createTask, getTasks } = require('../controllers/task.controller');
+const authMiddleware = require('../middleware/auth.middleware');
+
+router.post('/create-task', authMiddleware, createTask);
+router.get('/get-tasks', authMiddleware, getTasks);
+
+module.exports = router;
