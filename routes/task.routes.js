9205028e4e4e30add1918e686bdfa90e9de51745
joyagbo <joyagbo49@gmail.com>
2025-06-25 23:31:10 +0100

    add delete task route

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, getTasks } = require('../controllers/task.controller');
+const { createTask, getTasks, updateTask, deleteTask } = require('../controllers/task.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 
 router.post('/create-task', authMiddleware, createTask);
-router.get('/get-tasks', authMiddleware, getTasks);
+router.get('/', authMiddleware, getTasks);
+router.put('/:id', authMiddleware, updateTask);
+router.delete('/:id', authMiddleware, deleteTask);
+
 
 module.exports = router;
