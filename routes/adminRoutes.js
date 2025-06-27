dc11dcc511cbf31586076eb76a0f2567800f6b3e
joyagbo <joyagbo49@gmail.com>
2025-06-27 22:33:57 +0100

    add admin middleware

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
-const { getAllUsers, } = require('../controllers/adminController');
+const { getAllUsers, getAllTasks, deleteTask, deleteUser } = require('../controllers/adminController');
 
 router.get('/users', authMiddleware, adminMiddleware, getAllUsers);
+router.get('/tasks', authMiddleware, adminMiddleware, getAllTasks);
+router.delete('/task/:id', authMiddleware, adminMiddleware, deleteTask);
+router.delete('/user/:id', authMiddleware, adminMiddleware, deleteUser);
+
 
 
 module.exports = router;
