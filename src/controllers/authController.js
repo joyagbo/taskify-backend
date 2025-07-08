f8aa1ed2d66f4df2b1e482e751b90339babf996a
joyagbo <joyagbo49@gmail.com>
2025-07-08 03:39:03 +0100

    refactor: update authController to use authService for logic separation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,71 +1,32 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const authService = require('../services/auth.service');
+
 
 // Register a new user
 exports.register = async (req, res) => {
-    const { name, email, password } = req.body;
-
-    const details = ["name", "email", "password"];
-
-    for (const detail of details) {
-        if (!req.body[detail]) {
-            return res.status(400).json({ msg: `${detail} is required` });
-        }
-    }
 
     try {
-        // Check if user exists
-        const userExist = await User.findOne({ email });
-        if (userExist) return res.status(400).json({ message: "User already exists" });
+        const { userId } = await authService.registerUser(req.body);
 
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create new user
-        const newUser = await User.create({
-            name,
-            email,
-            password: hashedPassword,
-        });
         res.status(201).json({ message: "User registered successfully", userId: newUser._id });
     } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+        res.status(error.status || 500).json({ message: error.message || 'Server error' });
     }
 
 }
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
-
-    const details = ["email", "password"];
-
-    for (const detail of details) {
-        if (!req.body[detail]) {
-            return res.status(400).json({ msg: `${detail} is required` });
-        }
-    }
 
     try {
-        const user = await User.findOne({ email });
-        if (!user) return res.status(401).json({ message: "User not found" });
-        // Check password
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) return res.status(401).json({ message: "Invalid password" });
+        const { token, userId, role } = await authService.loginUser(req.body);
 
-        const token = jwt.sign(
-            { userId: user._id, role: user.role },
-            process.env.JWT_SECRET,
-            { expiresIn: '1d' }
-        )
         res.status(200).json({
             message: "Login successful",
             token,
-            userId: user._id,
-            role: user.role
+            userId,
+            role
         });
 
     } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+        res.status(error.status || 500).json({ message: error.message || 'Server error' });
     }
 }
