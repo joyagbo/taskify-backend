340ca592dec77f488223dad6893e353d7268c442
joyagbo <joyagbo49@gmail.com>
2025-07-08 03:37:35 +0100

    feat: add taskService to manage task creation and retrieval

---
+++ src/services/task.service.js
@@ -0,0 +1,45 @@
+const Task = require('../models/taskModel');
+
+
+exports.createTask = async (data) => {
+    return await Task.create(data);
+}
+
+exports.getTasks = async ({ userId, search, completed, page, limit }) => {
+    const query = { owner: userId };
+
+    if (completed !== undefined) {
+        query.completed = completed === 'true';
+    }
+
+
+    if (search) {
+        query.$or = [
+            { title: new RegExp(search, 'i') },
+            { description: new RegExp(search, 'i') }
+        ];
+    }
+
+    const tasks = await Task.find(query)
+        .sort({ createdAt: -1 })
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit));
+
+    const total = await Task.countDocuments(query);
+    return { tasks, total };
+}
+
+exports.getTaskById = async ({ taskId, userId }) => {
+    return await Task.findOne({ _id: taskId, owner: userId });
+}
+
+exports.updateTask = async ({ task, updates }) => {
+    Object.assign(task, updates);
+    return await task.save();
+}
+
+exports.deleteTask = async ({ taskId, userId }) => {
+    return await Task.findOneAndDelete({ _id: taskId, owner: userId });
+}
+
+
